29cb1315b94c523133361bb888127301553858c2
komuW <komuw05@gmail.com>
2021-05-15 13:19:48 +0300

    getting feet wet

--- example/main.go
+++ example/main.go
@@ -5,9 +5,12 @@ import (
 	"strconv"
 )
 
+type Distance float64
+
 type Fire struct {
 	Age  int64
 	Name string
+	Dist Distance
 }
 
 func (f Fire) Hello(i int) string {
@@ -18,11 +21,11 @@ func (f Fire) Hello(i int) string {
 go mod tidy
 make install
 /Users/komuw/go/bin/dlv debug example/main.go
-break example/main.go:26
+break example/main.go:29
 whatis f
 */
 func main() {
-	f := Fire{Age: 45, Name: "Komu"}
+	f := Fire{Age: 45, Name: "Komu", Dist: 4.8}
 	fmt.Println(f)
 	fmt.Println("hey")
 }
