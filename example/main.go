1c1b11f5e5e83e82ffa66186fae0299b0f70fdbf
komuW <komuw05@gmail.com>
2021-05-16 22:50:36 +0300

    getting feet wet

--- example/main.go
+++ example/main.go
@@ -3,29 +3,48 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"time"
 )
 
 type Distance float64
 
+type Health struct {
+	ID   uint64
+	Date time.Time
+}
+
 type Fire struct {
+	HH   Health
 	Age  int64
 	Name string
 	Dist Distance
+	Day  time.Time
 }
 
 func (f Fire) Hello(i int) string {
 	return strconv.Itoa(i)
 }
 
+func (f *Fire) MethodTwo() int64 {
+	return 78
+}
+
 /*
 go mod tidy
 make install
 /Users/komuw/go/bin/dlv debug example/main.go
-break example/main.go:29
+break example/main.go:48
 whatis f
 */
 func main() {
-	f := Fire{Age: 45, Name: "Komu", Dist: 4.8}
+	f := Fire{
+		HH:   Health{ID: 67, Date: time.Now()},
+		Age:  45,
+		Name: "Komu",
+		Dist: 4.8}
+	f.Hello(89) // If it is not called; then delve is not able to find it.
+	f.MethodTwo()
+
 	fmt.Println(f)
 	fmt.Println("hey")
 }
